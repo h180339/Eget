5dc0ca7537e9aa150e000a600c49d7614f243e5a
EirikAlv <eirik@example.com>
2018-03-16 16:25:53 +0100

    laget test

---
+++ Sortering/Test/no/hvl/dat102/SorteringTest.java
@@ -0,0 +1,27 @@
+package no.hvl.dat102;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SorteringTest {
+
+	@Test
+	public void SorteringVedInnsettingTest() {
+		SorteringVedInnsetting tabell = new SorteringVedInnsetting(10);
+		Integer i0 = 0;
+		Integer i2 = 1;
+		Integer i3 = 3;
+		Integer i4 = 4;
+		Integer i5 = 5;
+		Integer i6 = 6;
+		Integer i7 = 7;
+		Integer i8 = 8;
+
+		tabell.settInn(i0);
+
+
+
+	}
+
+}
--- Sortering/src/no/hvl/dat102/SorteringVedInnsetting.java
+++ Sortering/src/no/hvl/dat102/SorteringVedInnsetting.java
@@ -1,5 +1,40 @@
 package no.hvl.dat102;
 
-public class SorteringVedInnsetting {
+import static java.util.Arrays.binarySearch;
+
+public class SorteringVedInnsetting <T> {
+
+	T[] tab;
+	int antall;
+
+	public SorteringVedInnsetting (int antallPlasser) {
+
+		this.tab = (T[])(new Object[antallPlasser]);
+		this.antall = 0;
+
+	}
+	/*
+	public static void main (String [] args) {
+
+		Integer[] tab = new Integer[20];
+
+
+	}
+	*/
+
+	public void settInn (T el) {
+
+		if (antall == 0) { //setter inn første element
+			this.tab[0] = el;
+		}
+
+		int index = binarySearch(tab, el); //finner posisjon av
+		if (index >= 0) { //elementet er i tabellen allerede
+			System.out.println("Yes");
+		}
+
+
+	}
+
 
 }
