68c61e05b87d7efe39b6e01e970b452c5d4e8da8
EirikAlv <eirik@example.com>
2018-03-18 11:07:32 +0100

    laget sortering ved insetting etter å ha vært super retardert

--- Sortering/src/no/hvl/dat102/SorteringVedInnsetting.java
+++ Sortering/src/no/hvl/dat102/SorteringVedInnsetting.java
@@ -2,39 +2,51 @@ package no.hvl.dat102;
 
 import static java.util.Arrays.binarySearch;
 
-public class SorteringVedInnsetting <T> {
-
-	T[] tab;
-	int antall;
-
-	public SorteringVedInnsetting (int antallPlasser) {
-
-		this.tab = (T[])(new Object[antallPlasser]);
-		this.antall = 0;
-
-	}
-	/*
-	public static void main (String [] args) {
-
-		Integer[] tab = new Integer[20];
+public class SorteringVedInnsetting {
 
 
-	}
-	*/
+	public static  <T extends Comparable <T>> T[] sorter (T[] tab) {
 
-	public void settInn (T el) {
+		T[] hjelpeTab = (T[])(new Comparable[tab.length]);
+		hjelpeTab[0] = tab[0];
+		int antall = 1;
 
-		if (antall == 0) { //setter inn første element
-			this.tab[0] = el;
-		}
-
-		int index = binarySearch(tab, el); //finner posisjon av
-		if (index >= 0) { //elementet er i tabellen allerede
-			System.out.println("Yes");
+		for (int i = 1; i < tab.length; i++) {
+			hjelpeTab = settInn(hjelpeTab, tab[i], antall);
+			antall++;
 		}
 
 
+		/*for (int v = 0; v < hjelpeTab.length; v++) {
+			System.out.print(hjelpeTab[v]);
+		}*/
+
+		return hjelpeTab;
 	}
+	private static <T extends Comparable <T>> T[] settInn(T[] tab, T el, int antall) {
+
+		int index = 0;
+
+		while(index < antall && el.compareTo(tab[index]) > 0) {
+			index++;
+		}
+		//System.out.println("index : " + index + " antall : " + antall + " element: " + el);
+		//System.out.println(tab.length);
+		for (int i = antall - 1; i >= index; i--) {
+		//	System.out.println("i: " + i);
+			tab[i + 1] = tab[i];
+		}
+		tab[index] = el;
+
+		/*for (int v = 0; v < tab.length; v++) {
+			System.out.print(tab[v]);
+		}
+		System.out.println();*/
+		return tab;
+	}
+
+
+
 
 
 }
